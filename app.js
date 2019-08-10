f3c0fead9d69dba55eda971c6d7ef0f13f1ee32d
Liam Dawson <liamrdawson@gmail.com>
2019-08-10 14:09:46 +0100

    Create and compose components, ReactDOM.render App

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
-const Header = () => (
-    <header>
-        <h1>Scoreboard</h1>
-        <span className="stats">Players: 1</span>
-    </header>
-);
+const Header = () => {
+    return (
+        <header>
+            <h1>Fantasy Football Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Liam
+            </span>
+            <Counter />
+        </div>
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score"> 21</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+
+            {/*Player List*/}
+            <Player />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    <Header />,
+    <App />,
     document.getElementById('root')
 );
