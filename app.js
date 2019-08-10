5ced185890adcf4743b374a19327d6bc527f4c72
Liam Dawson <liamrdawson@gmail.com>
2019-08-10 10:21:26 +0100

    convert components to arrow functionsto keep code succinct

---
+++ app.js
@@ -0,0 +1,12 @@
+const Header = () => (
+    <header>
+        <h1>Scoreboard</h1>
+        <span className="stats">Players: 1</span>
+    </header>
+);
+
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+);
