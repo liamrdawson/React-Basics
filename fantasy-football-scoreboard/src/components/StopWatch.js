9e91cbf1e46756ca94bfb0f333894e01358d25e9
Liam Dawson <liamrdawson@gmail.com>
2019-08-15 21:12:45 +0100

    start handleTimer if isRunning state === true

--- fantasy-football-scoreboard/src/components/StopWatch.js
+++ fantasy-football-scoreboard/src/components/StopWatch.js
@@ -1,12 +1,46 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+
+    state = {
+        isRunning: false,
+        elapsedTime: 0,
+        previousTime: 0
+    }
+
+    componentDidMount() {
+        this.intervalID = setInterval( () => this.tick(), 100); 
+    }
+
+    tick = () => {
+        if (this.state.isRunning) {
+            const now = Date.now();
+            this.setState( prevState => ({
+                    previousTime: now,
+                    elapsedTime: prevState.elapsedTime + (now - this.state.previousTime)
+                }));
+        }
+    }
+
+    handleTimer = () => {
+        this.setState( prevState => ({
+                isRunning: !this.state.isRunning
+        }));
+        if (!this.state.isRuning) {
+            this.setState({
+                    previousTime: Date.now()
+                });
+        }
+    }
+
     render() {
         return (
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
                 <span className="stopwatch-time">0</span>
-                <button>Start</button>
+                <button onClick={this.handleTimer}>
+                    {this.state.isRunning ? 'Stop' : 'Start'}
+                </button>
                 <button>Reset</button>
             </div>
         );
